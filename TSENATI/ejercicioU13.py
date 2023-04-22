5be21edbf7dae21ba33c53fddbd484ad60323b9c
Fernando <65874875+fernandoinoue@users.noreply.github.com>
2023-04-21 23:02:15 -0500

    Update ejercicioU13.py

--- TSENATI/ejercicioU13.py
+++ TSENATI/ejercicioU13.py
@@ -10,14 +10,14 @@
 
 
 # Crea una tupla de 10 números e imprime el mayor y el menor. 
-print("Digite numero 1 a 10")
-n = input("Agreg maximo 10: ")
-maximo_n = n[0]
-minimo_n =n[0]
-for i in n:
-    if i > maximo_n:
-        maximo_n = n
-    if i < minimo_n:
-        minimo_n = n
-    print("Maximo numero es", maximo_n, sep=" ")
-    print("Maximo numero es", minimo_n, sep=" ")
+# print("Digite numero 1 a 10")
+# n = input("Agreg maximo 10: ")
+# maximo_n = n[0]
+# minimo_n =n[0]
+# for i in n:
+#     if i > maximo_n:
+#         maximo_n = n
+#     if i < minimo_n:
+#         minimo_n = n
+#     print("Maximo numero es", maximo_n, sep=" ")
+#     print("Maximo numero es", minimo_n, sep=" ")
