b7150f5775adae6b6805a529b942217675014422
Fernando <65874875+fernandoinoue@users.noreply.github.com>
2023-04-21 23:03:01 -0500

    Update ejercicioU14.py

--- TSENATI/ejercicioU14.py
+++ TSENATI/ejercicioU14.py
@@ -1,5 +1,5 @@
-import random 
-print("10 primeros numeros: ")
-n=(10)
-al_azar = [random.randint(0,10)]
-print("Numero al azar: ", al_azar)
+# import random 
+# print("10 primeros numeros: ")
+# n=(10)
+# al_azar = [random.randint(0,10)]
+# print("Numero al azar: ", al_azar)
